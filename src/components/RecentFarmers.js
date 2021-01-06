c8d081ab5d4f9288d7d042f668f3254ece372212
github username <github email address>
2021-01-05 19:07:16 -0500

    confirmCartHandler-notPaid-debugger

--- src/components/RecentFarmers.js
+++ src/components/RecentFarmers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react'
+import { Grid, Label, Image, Table } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom';
 import RecentCard from '../cards/ReceiptCard.js';
@@ -16,8 +16,22 @@ recentFarmerIterator = () => {
     let farmerIds = targetItems.filter(element => element.farmer_id)
     let targetFarmers = this.props.farmers.filter(element => farmerIds.includes(element.id))
 
+    if (targetFarmers === []) {
+        // debugger
     return (
         targetFarmers.map(element => <RecentCard key={element.id} id={element.id} />)
+    )
+    } else {
+       return this.emptyFarmers();
+    }
+}
+
+emptyFarmers = () => {
+    return (
+            <>
+                <Label  color='grey' size='huge'>Mooooooo... Looks like you haven't purchased anything, yet. Sorry :(</Label>
+                <Image size='huge' src={'https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/feed/feednavigator.com/article/2020/05/19/the-cow-favors-natural-over-synthetic-vitamin-e/11403541-1-eng-GB/The-cow-favors-natural-over-synthetic-vitamin-E.jpg'}/>
+            </>
     )
 }
 
@@ -27,7 +41,7 @@ recentFarmerIterator = () => {
             <Grid divided='vertically'>
             <Grid.Row columns={1}>
                 <Grid.Column>
-                    {this.recentFarmerIterator()}
+                    {this.props.itemOrders? this.recentFarmerIterator() : this.emptyFarmers}
                 </Grid.Column> 
             
             </Grid.Row>
