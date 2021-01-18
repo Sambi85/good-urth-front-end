5bdfc49a4d40571fe5902ce91ed34fdcca921f6f
github username <github email address>
2021-01-17 20:27:13 -0500

    clean-up-actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,7 +5,7 @@ export const getMarkets = (marketObj) => {
         // console.log("nested function invoked")
         fetch('http://localhost:4000/markets')
         .then(resp => resp.json())
-        // .then(console.log)
+
         .then(data => dispatch({type: "fetched markets", payload: data}) )
     }
 }
